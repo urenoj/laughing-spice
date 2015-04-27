b555e2bb28c065c20cd13b794a4ffb3422ec49aa
urenoj <jureno95@gmail.com>
2015-04-27 15:15:17 -0700

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,12 +1,11 @@
-# include <iostream>
-# include <cstdlib>
-# include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-int main()
-{
-    unsigned int seed = time(0);      // random number generator for when X can't win or block
+int main() {
+    unsigned int seed = time(0);      // Random number generator for when X can't win or block
     srand(seed);
     
     bool madeMove = 0;
@@ -17,7 +16,7 @@ int main()
     for (int i = 0; i < 9; i++)
         cin >> boardState[i];
     
-    // this generates the tic tac toe board
+    // This generates the Tic Tac Toe board
     
     cout << "Current board state: " << endl;
     cout << "+-+-+-+" << endl;
@@ -28,44 +27,30 @@ int main()
     cout << "|" << boardState[6] << "|" << boardState[7] << "|" << boardState[8] << "|" << endl;
     cout << "+-+-+-+" << endl;
     
-    
-    
     /*
-     
-     
-     
      
      WINNING MOVES
      
-     
-     
-     
      */
     
-    if (boardState[0] == 0 && madeMove == 0)
-    {
-        if ((boardState[3] == 1 && boardState[6] == 1) || (boardState[4] == 1  && boardState[8] == 1) || (boardState[1] == 1 && boardState[2] == 1))
-        {
+    if (boardState[0] == 0 && madeMove == 0) {
+        if ((boardState[3] == 1 && boardState[6] == 1) || (boardState[4] == 1  && boardState[8] == 1) || (boardState[1] == 1 && boardState[2] == 1)) {
             boardState[0] = 1;
             cout << "X takes the winning move on cell 1!" << endl;
             madeMove = 1;
         }
-        
     }
-    else if (boardState[1] == 0 && madeMove == 0)
-    {
-        if ((boardState[4] == 1 && boardState[7] == 1) || (boardState[0] == 1 && boardState[2] == 1))
-        {
+    
+    else if (boardState[1] == 0 && madeMove == 0) {
+        if ((boardState[4] == 1 && boardState[7] == 1) || (boardState[0] == 1 && boardState[2] == 1)) {
             boardState[1] = 1;
             cout << "X takes the winning move on cell 2!" << endl;
             madeMove = 1;
         }
     }
     
-    else if (boardState[2] == 0 && madeMove == 0)
-    {
-        if ((boardState[4] == 1 && boardState[6] == 1) || (boardState[5] == 1 && boardState[8] == 1) || (boardState[0] == 1 && boardState[1] == 1))
-        {
+    else if (boardState[2] == 0 && madeMove == 0) {
+        if ((boardState[4] == 1 && boardState[6] == 1) || (boardState[5] == 1 && boardState[8] == 1) || (boardState[0] == 1 && boardState[1] == 1)) {
             boardState[2] = 1;
             cout << "X takes the winning move on cell 3!" << endl;
             madeMove = 1;
@@ -73,48 +58,36 @@ int main()
     }
     
     /* 
-     
-     
-     
-     
+
      BLOCKING MOVES
-     
-     
-     
-     
+    
      */
     
-    if (boardState[0] == 0 && madeMove == 0)
-    {
-        if ((boardState[3] == 2 && boardState[6] == 2) || (boardState[4] == 2 && boardState[8] == 2) || (boardState[1] == 2 && boardState[2] == 2))
-        {
+    if (boardState[0] == 0 && madeMove == 0) {
+        if ((boardState[3] == 2 && boardState[6] == 2) || (boardState[4] == 2 && boardState[8] == 2) || (boardState[1] == 2 && boardState[2] == 2)) {
             boardState[0] = 1;
             cout << "X blocks the winning move on cell 1!" << endl;
             madeMove = 1;
         }
     }
     
-    else if (boardState[1] == 0 && madeMove == 0)
-    {
-        if ((boardState[4] == 2 && boardState[7] == 2) || (boardState[0] == 2 && boardState[2] == 2))
-        {
+    else if (boardState[1] == 0 && madeMove == 0) {
+        if ((boardState[4] == 2 && boardState[7] == 2) || (boardState[0] == 2 && boardState[2] == 2)) {
             boardState[1] = 1;
             cout << "X blocks the winning move on cell 2!" << endl;
             madeMove = 1;
         }
     }
-    else if (boardState[2] == 0 && madeMove == 0)
-    {
-        if ((boardState[4] == 2 && boardState[6] == 2) || (boardState[5] == 2 && boardState[8] == 2) || (boardState[0] == 2 && boardState[1] == 2))
-        {
+    
+    else if (boardState[2] == 0 && madeMove == 0) {
+        if ((boardState[4] == 2 && boardState[6] == 2) || (boardState[5] == 2 && boardState[8] == 2) || (boardState[0] == 2 && boardState[1] == 2)) {
             boardState[2] = 1;
             cout << "X blocks the winning move on cell 3!" << endl;
             madeMove = 1;
         }
     }
     
-    if (madeMove == 0)
-    {
+    if (madeMove == 0) {
         num = rand() % 3;
         
         while (boardState[num] == 1 || boardState[num] == 2) {
@@ -124,6 +97,7 @@ int main()
         boardState[num] = 1;
         cout << "X chose cell " << (num + 1)  << "!" << endl;
     }
+    
     cout << "New board:" << endl;
     cout << "+-+-+-+" << endl;
     cout << "|" << boardState[0] << "|" << boardState[1] << "|" << boardState[2] << "|" << endl;
